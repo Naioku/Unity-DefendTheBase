2527677aa0f70da23a9bd11b229ad0b90b6025be
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-09-21 11:40:19 +0200

    Set up locomotion animations.

--- DefendTheBase/Assets/Scripts/StateMachines/Player/PlayerLocomotionState.cs
+++ DefendTheBase/Assets/Scripts/StateMachines/Player/PlayerLocomotionState.cs
@@ -4,11 +4,15 @@ namespace StateMachines.Player
 {
     public class PlayerLocomotionState : PlayerBaseState
     {
+        private static readonly int LocomotionStateHash = Animator.StringToHash("Locomotion");
+        private static readonly int ForwardMovementSpeedHash = Animator.StringToHash("ForwardMovementSpeed");
+        private static readonly int RightMovementSpeedHash = Animator.StringToHash("RightMovementSpeed");
+        
         public PlayerLocomotionState(PlayerStateMachine stateMachine) : base(stateMachine) {}
     
         public override void Enter()
         {
-            
+            StateMachine.Animator.CrossFadeInFixedTime(LocomotionStateHash, StateMachine.AnimationCrossFadeDuration);
         }
 
         public override void Tick(float deltaTime)
@@ -16,6 +20,33 @@ namespace StateMachines.Player
             var movementDirection = CalculateMovementDirectionFromCameraPosition();
             StateMachine.PlayerMover.Move(movementDirection, deltaTime);
             StateMachine.PlayerMover.FaceCameraForward(deltaTime);
+            UpdateAnimator(deltaTime);
+        }
+
+        private void UpdateAnimator(float deltaTime)
+        {
+            float movementRightValue = StateMachine.InputReader.MovementValue.x;
+            float movementForwardValue = StateMachine.InputReader.MovementValue.y;
+
+            if (movementForwardValue == 0f)
+            {
+                StateMachine.Animator.SetFloat(ForwardMovementSpeedHash, 0, StateMachine.AnimatorDampTime, deltaTime);
+            }
+            else
+            {
+                float value = movementForwardValue > 0f ? 1f : -1f;
+                StateMachine.Animator.SetFloat(ForwardMovementSpeedHash, value, StateMachine.AnimatorDampTime, deltaTime);
+            }
+            
+            if (movementRightValue == 0f)
+            {
+                StateMachine.Animator.SetFloat(RightMovementSpeedHash, 0, StateMachine.AnimatorDampTime, deltaTime);
+            }
+            else
+            {
+                float value = movementRightValue > 0f ? 1f : -1f;
+                StateMachine.Animator.SetFloat(RightMovementSpeedHash, value, StateMachine.AnimatorDampTime, deltaTime);
+            }
         }
 
         public override void Exit()
--- DefendTheBase/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ DefendTheBase/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -1,17 +1,23 @@
 using InputSystem;
 using Locomotion;
+using UnityEngine;
 
 namespace StateMachines.Player
 {
     public class PlayerStateMachine : StateMachine
     {
+        [field: SerializeField] public float AnimationCrossFadeDuration { get; private set; } = 0.1f;
+        [field: SerializeField] public float AnimatorDampTime { get; private set; } = 0.05f;
+        
         public InputReader InputReader { get; private set; }
         public PlayerMover PlayerMover { get; private set; }
+        public Animator Animator { get; private set; }
 
         private void Awake()
         {
             InputReader = GetComponent<InputReader>();
             PlayerMover = GetComponent<PlayerMover>();
+            Animator = GetComponent<Animator>();
         }
 
         private void Start()
