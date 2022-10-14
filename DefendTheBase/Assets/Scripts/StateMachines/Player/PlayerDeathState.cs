5bf7e465e8ec3d1760257a80bd518399a4d25fd2
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-10-14 14:42:26 +0200

    Player death state set up.

---
+++ DefendTheBase/Assets/Scripts/StateMachines/Player/PlayerDeathState.cs
@@ -0,0 +1,24 @@
+using StateMachines.Player.Knight;
+using UnityEngine;
+
+namespace StateMachines.Player
+{
+    public class PlayerDeathState : PlayerBaseState
+    {
+        private static readonly int BlockStateHash = Animator.StringToHash("Death");
+
+        public PlayerDeathState(KnightStateMachine stateMachine) : base(stateMachine) {}
+        
+        public override void Enter()
+        {
+            StateMachine.Animator.CrossFadeInFixedTime(BlockStateHash, StateMachine.AnimationCrossFadeDuration);
+            StateMachine.PlayerMover.DisableCharacterController();
+        }
+
+        public override void Tick() {}
+
+        public override void Exit()
+        {
+        }
+    }
+}
